916f8971663a45662cb46907c5224f87a86fc200
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-03 14:29:31 -0600

    Launch docker with multiple mapped directories

--- testing/direct_call/0_start_docker.sh
+++ testing/direct_call/0_start_docker.sh
@@ -1,5 +1,6 @@
-# Start docker container in regular and/or MGI environment, and map given path to /data
-# Usage: 0_start_docker.sh [options] data_path
+#!/bin/bash
+# Start docker container in regular and/or MGI environment, and map given path to /data1, /data2, ...
+# Usage: 0_start_docker.sh [options] data_path_1 [data_path_2 ...]
 #
 # -M: run in MGI environment
 # -d: dry run.  print out docker statement but do not execute
@@ -39,27 +40,44 @@ while getopts ":MdI:" opt; do
 done
 shift $((OPTIND-1))
 
-if [ "$#" -ne 1 ]; then
-    >&2 echo Usage: 0_start_docker.sh data_path
+if [ "$#" -lt 1 ]; then
+    >&2 echo Usage: 0_start_docker.sh data1_path \[data2_path...\]
     exit 1
 fi
 
-DATD=$1
-if [ ! -d $DATD ]; then
-    >&2 echo Error: $DATD is not an existing directory
-    exit 1
-fi
+D=1
+DATMAP=""
+# Loop over all arguments, host directories which will be mapped to container directories
+for DATDH in "$@"
+do
+    if [ ! -d $DATDH ]; then
+        >&2 echo Error: $DATDH is not an existing directory
+        exit 1
+    fi
 
-# Using python to get absolute path of DATD.  On Linux `readlink -f` works, but on Mac this is not always available
-# see https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
-ADATD=$(python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' $DATD)
-echo Mapping /data to $ADATD
+    # Using python to get absolute path of DATDH.  On Linux `readlink -f` works, but on Mac this is not always available
+    # see https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+    ADATDH=$(python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' $DATDH)
+
+    # container data directories are /data1, /data2, ...
+    DATDC="/data${D}" 
+
+    >&2 echo Mapping $DATDC to $ADATDH
+    if [ $MGI ]; then
+        DATMAP="$DATMAP $ADATDH:$DATDC"
+    else
+        DATMAP="$DATMAP -v $ADATDH:$DATDC"
+    fi 
+
+    let D++
+done
 
 # MGI code from https://github.com/ding-lab/importGDC/blob/master/GDC_import.sh
 function start_docker_MGI {
 
 # Where container's /data is mounted on host
-export LSF_DOCKER_VOLUMES="$ADATD:/data"
+#export LSF_DOCKER_VOLUMES="$ADATD:/data"
+export LSF_DOCKER_VOLUMES="$DATMAP"
 
 CMD="bsub $LSFQ -Is -a \"docker($DOCKER_IMAGE)\" /bin/bash "
 if [ $DRYRUN ]; then
@@ -76,7 +94,7 @@ function start_docker {
 #JAVA_OPTS="-Xms512m -Xmx512m"
 #ENVARGS="-e JAVA_OPTS=\"$JAVA_OPTS\""
 
-CMD="docker run $ENVARGS -v $ADATD:/data -it $DOCKER_IMAGE /bin/bash"
+CMD="docker run $ENVARGS $DATMAP -it $DOCKER_IMAGE /bin/bash"
 if [ $DRYRUN ]; then
     echo Dryrun: $CMD
 else
