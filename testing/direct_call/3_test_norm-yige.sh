b7b39de662e958edfbe8333e4b6f343451a5dceb
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-02 18:00:51 -0600

    Ad hoc testing script
    * checking in so is preserved when image closes

---
+++ testing/direct_call/3_test_norm-yige.sh
@@ -0,0 +1,7 @@
+# Debugging calls related to work with Yige.
+# From MGI: /gscmnt/gc2521/dinglab/yigewu/Projects/CPTAC3CNV/BICSEQ2/outputs/CCRCC.hg38.test/run_norm/C3L-00004_tumor_commands.sh
+
+# Yige original call on MGI:
+# perl /NBICseq-norm_v0.2.4/NBICseq-norm.pl --tmp=/gscmnt/gc2521/dinglab/yigewu/Projects/CPTAC3CNV/BICSEQ2/outputs/CCRCC.hg38.test/run_norm/TMP/ -l 150 -s 350 -b 100 --fig /gscmnt/gc2521/dinglab/yigewu/Projects/CPTAC3CNV/BICSEQ2/outputs/CCRCC.hg38.test/run_norm/C3L-00004_tumor_GC.pdf /gscmnt/gc2521/dinglab/yigewu/Projects/CPTAC3CNV/BICSEQ2/outputs/CCRCC.hg38.test/run_norm/C3L-00004_tumor_config.txt /gscmnt/gc2521/dinglab/yigewu/Projects/CPTAC3CNV/BICSEQ2/outputs/CCRCC.hg38.test/run_norm/C3L-00004_tumor_out.txt
+
+echo perl /NBICseq-norm_v0.2.4/NBICseq-norm.pl --tmp=/data/tmp/ -l 150 -s 350 -b 100 --fig=/data/testout/C3L-00004_tumor_GC.pdf /data/C3L-00004_tumor_config-remapped.txt /data/testout/C3L-00004_tumor_out.txt
