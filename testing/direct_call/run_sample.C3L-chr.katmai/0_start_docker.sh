8f7a21adc720f5a07282ed49d2825ec39d5ad0ed
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-06 00:17:24 +0000

    Re-organizing / standardizing directory mappings
    * Will use self-generated gene annotation file for testing
      rather than one from Yige

--- testing/direct_call/run_sample.C3L-chr.katmai/0_start_docker.sh
+++ testing/direct_call/run_sample.C3L-chr.katmai/0_start_docker.sh
@@ -2,10 +2,9 @@ BICSEQ2="/home/mwyczalk_test/Projects/BICSEQ2"
 
 # See README.md for details.  Paths specific to katmai
 bash $BICSEQ2/src/start_docker.sh  \
-    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/inputs  \
-    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/outputs/UCEC.hg38.test/run_uniq  \
     /diskmnt/Datasets/BICSEQ2-dev.tmp \
-    /diskmnt/Projects/CPTAC3CNV/gatk4wxscnv/inputs 
+    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/inputs  \
+    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/outputs/UCEC.hg38.test/run_uniq  
 
 # Note that we are reusing Yige's run UCEC.hg38.test for run_uniq
 # This is the same as her subsequent runs, e.g., BICSEQ2.UCEC.hg38.121
