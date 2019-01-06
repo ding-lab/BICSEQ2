5726471efd5775f71db1a34f4220f910303e190d
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-05 19:45:30 -0600

    Adding get_unique step to run_sample.C3L-chr.katmai project
    * This involves mounting volume which has BAM files
    * Removing Yige's previous run from mounted volumes
    * Using cached .seq data may be preferable for testing purposes
      so may want to revert this back for some projects

--- testing/direct_call/run_sample.C3L-chr.katmai/0_start_docker.sh
+++ testing/direct_call/run_sample.C3L-chr.katmai/0_start_docker.sh
@@ -4,7 +4,7 @@ BICSEQ2="/home/mwyczalk_test/Projects/BICSEQ2"
 bash $BICSEQ2/src/start_docker.sh  \
     /diskmnt/Datasets/BICSEQ2-dev.tmp \
     /diskmnt/Projects/CPTAC3CNV/BICSEQ2/inputs  \
-    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/outputs/UCEC.hg38.test/run_uniq  
+    /diskmnt/Projects/cptac_downloads_3/GDC_import/data \
 
 # Note that we are reusing Yige's run UCEC.hg38.test for run_uniq
 # This is the same as her subsequent runs, e.g., BICSEQ2.UCEC.hg38.121
