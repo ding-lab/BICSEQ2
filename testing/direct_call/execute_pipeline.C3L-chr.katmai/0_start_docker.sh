480c8df21f53fb18f8f7f4432076eacc0f37b851
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-08 06:12:43 +0000

    Adding gene annotation file volume

--- testing/direct_call/execute_pipeline.C3L-chr.katmai/0_start_docker.sh
+++ testing/direct_call/execute_pipeline.C3L-chr.katmai/0_start_docker.sh
@@ -1,15 +1,13 @@
 BICSEQ2="/home/mwyczalk_test/Projects/BICSEQ2"
 
-#  This is if want to use external SEQ files
-#   data4:/diskmnt/Projects/CPTAC3CNV/BICSEQ2/outputs/UCEC.hg38.test/run_uniq  (.seq files)
-#  If so, define $IMPORT_SEQ; otherwise, SEQ obtained from pipeline 
-#	currently, this is defined in project configuration file
+# gene annotation file:
+# /diskmnt/Projects/CPTAC3CNV/gatk4wxscnv/inputs/gencode.v29.annotation.hg38.p12.protein_coding.bed
 
 # See README.md for details.  Paths specific to katmai
 bash $BICSEQ2/src/start_docker.sh  \
     /diskmnt/Datasets/BICSEQ2-dev.tmp \
     /diskmnt/Projects/CPTAC3CNV/BICSEQ2/inputs  \
     /diskmnt/Projects/cptac_downloads_5/GDC_import/data \
-    /diskmnt/Projects/CPTAC3CNV/BICSEQ2/outputs/UCEC.hg38.test/run_uniq
+    /diskmnt/Projects/CPTAC3CNV/gatk4wxscnv/inputs
 
 # Tip: run this command within a tmux session for long runs
