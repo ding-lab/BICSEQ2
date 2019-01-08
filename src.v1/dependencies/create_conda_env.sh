d497a1f2e708fa887a9bde9c4bd9bea406870c4a
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-01-07 21:28:00 -0600

    Successful run of complete workflow on C3L-00008
    * Getting rid of v1 code

--- src.v1/dependencies/create_conda_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda create -n bicseq2
-conda install -c conda-forge parallel -n bicseq2
-conda install -c r r -n bicseq2
-conda install -c bioconda bedtools -n bicseq2
-conda install -c conda-forge numpy -n bicseq2
--- src.v1/dependencies/install_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#install docker
-sudo apt-get update
-sudo apt-get install docker-ce
-sudo usermod -a -G docker ${USER}
-exit
