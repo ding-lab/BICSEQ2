d6626e0106c6852130f22110254dbf7f03c0c3b9
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-01-09 14:23:44 -0600

    Not keeping user environment in MGI environment

--- src/start_docker.sh
+++ src/start_docker.sh
@@ -81,8 +81,8 @@ CMD="$1"
 #export LSF_DOCKER_VOLUMES="$ADATD:/data"
 export LSF_DOCKER_VOLUMES="$DATMAP"
 
-# This clears most environment variables / paths, but also clears $HOME, which parallel relies on
-export LSF_DOCKER_PRESERVE_ENVIRONMENT=false
+## This clears most environment variables / paths, but also clears $HOME, which parallel relies on
+#export LSF_DOCKER_PRESERVE_ENVIRONMENT=false
 
 DCMD="bsub $LSFQ -Is -a \"docker($DOCKER_IMAGE)\" $CMD "
 if [ $DRYRUN ]; then
