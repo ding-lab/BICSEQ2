37ee36bf023b74b9222909946acff2a064fa8d45
yigewu <yigewu@wustl.edu>
2019-03-22 15:54:27 -0500

    Y2.b1.v1

---
+++ testing/docker_call/run_cases.Y2.b1.katmai/3_make_case_list.sh
@@ -0,0 +1,11 @@
+source project_config-host.sh
+
+CASES="dat/case_names.dat"
+
+DOCKERMAP="dat/Dockermap.dat"
+OUT="dat/CaseList.dat"
+
+#bash $BICSEQ_H/src/make_case_list.sh -b $BAMMAP -m $DOCKERMAP - < $CASES > $OUT
+bash src/make_case_list.sh -b $BAMMAP -m $DOCKERMAP - < $CASES > $OUT
+
+echo Written to $OUT
---
+++ testing/docker_call/run_cases.Y2.b1.katmai/C.make_analysis_description.sh
@@ -0,0 +1,10 @@
+source project_config-host.sh
+
+CASES="dat/case_names.dat"
+
+DOCKERMAP="dat/Dockermap.dat"
+OUT="dat/"$BATCH_NAME".analysis_description.dat"
+
+bash src/make_analysis_description.sh -b $BAMMAP -O $OUTD_H -m $DOCKERMAP - < $CASES > $OUT
+
+echo Written to $OUT
